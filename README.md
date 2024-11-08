a17a9e173af428ad6f981ee46ee526ddf953c194
Max Brunsfeld <maxbrunsfeld@gmail.com>
2024-11-08 13:13:36 -0800

    Initial commit
    Co-authored-by: Marshall <marshall@zed.dev>

---
+++ README.md
@@ -0,0 +1,3 @@
+# postgres-context-server
+
+This module implements a Model Context Protocol server for a PostgreSQL database.
---
+++ index.mjs
@@ -0,0 +1,194 @@
+#!/usr/bin/env node
+
+import pg from "pg";
+import { Server } from "./vendor/modelcontextprotocol-typescript-sdk/dist/server/index.js";
+import { StdioServerTransport } from "./vendor/modelcontextprotocol-typescript-sdk/dist/server/stdio.js";
+import {
+  CallToolRequestSchema,
+  ListResourcesRequestSchema,
+  ListPromptsRequestSchema,
+  ListToolsRequestSchema,
+  ReadResourceRequestSchema,
+  GetPromptRequestSchema,
+} from "./vendor/modelcontextprotocol-typescript-sdk/dist/types.js";
+
+const server = new Server({
+  name: "example-servers/postgres",
+  version: "0.1.0",
+});
+
+const args = process.argv.slice(2);
+if (args.length === 0) {
+  console.error("Please provide a database URL as a command-line argument");
+  process.exit(1);
+}
+
+const databaseUrl = args[0];
+
+const resourceBaseUrl = new URL(databaseUrl);
+resourceBaseUrl.protocol = "postgres:";
+resourceBaseUrl.password = "";
+
+process.stderr.write("starting server. url: " + databaseUrl + "\n");
+const pool = new pg.Pool({
+  connectionString: databaseUrl,
+});
+
+const SCHEMA_PATH = "schema";
+
+server.setRequestHandler(ListResourcesRequestSchema, async () => {
+  const client = await pool.connect();
+  try {
+    const result = await client.query(
+      "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'",
+    );
+    return {
+      resources: result.rows.map((row) => ({
+        uri: new URL(`${row.table_name}/${SCHEMA_PATH}`, resourceBaseUrl).href,
+        mimeType: "application/json",
+        name: `"${row.table_name}" database schema`,
+      })),
+    };
+  } finally {
+    client.release();
+  }
+});
+
+server.setRequestHandler(ReadResourceRequestSchema, async (request) => {
+  const resourceUrl = new URL(request.params.uri);
+
+  const pathComponents = resourceUrl.pathname.split("/");
+  const schema = pathComponents.pop();
+  const tableName = pathComponents.pop();
+
+  if (schema !== SCHEMA_PATH) {
+    throw new Error("Invalid resource URI");
+  }
+
+  const client = await pool.connect();
+  try {
+    const result = await client.query(
+      "SELECT column_name, data_type FROM information_schema.columns WHERE table_name = $1",
+      [tableName],
+    );
+
+    return {
+      contents: [
+        {
+          uri: request.params.uri,
+          mimeType: "application/json",
+          text: JSON.stringify(result.rows, null, 2),
+        },
+      ],
+    };
+  } finally {
+    client.release();
+  }
+});
+
+server.setRequestHandler(ListToolsRequestSchema, async () => {
+  return {
+    tools: [
+      {
+        name: "query",
+        description: "Run a read-only SQL query",
+        inputSchema: {
+          type: "object",
+          properties: {
+            sql: { type: "string" },
+          },
+        },
+      },
+    ],
+  };
+});
+
+server.setRequestHandler(CallToolRequestSchema, async (request) => {
+  if (request.params.name === "query") {
+    const sql = request.params.arguments?.sql;
+
+    const client = await pool.connect();
+    try {
+      await client.query("BEGIN TRANSACTION READ ONLY");
+      const result = await client.query(sql);
+      return { toolResult: result.rows };
+    } catch (error) {
+      throw error;
+    } finally {
+      client
+        .query("ROLLBACK")
+        .catch((error) =>
+          console.warn("Could not roll back transaction:", error),
+        );
+
+      client.release();
+    }
+  }
+  throw new Error("Tool not found");
+});
+
+server.setRequestHandler(ListPromptsRequestSchema, async () => {
+  process.stderr.write("Handling prompts/list request\n");
+
+  return {
+    prompts: [
+      {
+        name: "postgres-table-schema",
+        description:
+          "Retrieve the schema for a given table in the postgres database",
+        arguments: [
+          {
+            name: "tableName",
+            description: "the table to describe",
+            required: true,
+          },
+        ],
+      },
+    ],
+  };
+});
+
+server.setRequestHandler(GetPromptRequestSchema, async (request) => {
+  process.stderr.write("Handling prompts/get request\n");
+
+  if (request.params.name === "postgres-table-schema") {
+    const tableName = request.params.arguments?.tableName;
+    if (typeof tableName !== "string" || tableName.length === 0) {
+      throw new Error(`Invalid tableName: ${tableName}`);
+    }
+
+    const client = await pool.connect();
+    try {
+      const result = await client.query(
+        "SELECT column_name, data_type FROM information_schema.columns WHERE table_name = $1",
+        [tableName],
+      );
+
+      return {
+        messages: [
+          {
+            role: "user",
+            content: {
+              type: "text",
+              text: JSON.stringify(result.rows, null, 2),
+            },
+          },
+        ],
+      };
+    } finally {
+      client.release();
+    }
+  }
+
+  throw new Error(`Prompt '${request.params.name}' not implemented`);
+});
+
+async function runServer() {
+  const transport = new StdioServerTransport();
+  await server.connect(transport);
+}
+
+runServer().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
